2876b5f927361a0106b1876be3be9be86f3e6458
ushodaya1303 <87666428+ushodaya1303@users.noreply.github.com>
2022-06-08 19:39:32 +0530

    Update Header.jsx

--- apps/src/Header.jsx
+++ apps/src/Header.jsx
@@ -1,8 +1,10 @@
 import React from "react";
  const Header =()=>{
+  console.log("hi");
      return(
          <div style={{textAlign: "center"}}>
      <h1>contact page</h1>
+
      </div>
      );
  }
