c3aaf373305ce9a2155c7b169eb1a1d2437bf0ba
abhinavnmishra <57250845+abhinavnmishra@users.noreply.github.com>
2022-06-10 15:19:33 +0530

    adding api

--- apps/src/addcontact.jsx
+++ apps/src/addcontact.jsx
@@ -1,32 +1,64 @@
-
 import React from "react";
 import './index.css'
 import Typography from '@mui/material/Typography'
-
+var axios = require('axios');
 
 
 class  Addcontact extends React.Component{
      state={
-        name:"",
+        password:"",
         email:""
     };
+
  add=(e)=>{
         e.preventDefault();
-        if(this.state.name==="" || this.state.email===""){
-            alert("please enter valid detials");
+        if(this.state.password==="" || this.state.email===""){
+            alert("please enter valid details");
             return;
         }
        this.props.addContactListener(this.state);
-       this.setState({name:"",email:""});
+
+     var data = JSON.stringify({
+         "email": this.state.email,
+         "password": this.state.password
+     });
+
+
+     var config = {
+         method: 'post',
+         url: 'https://cfg22-backend.herokuapp.com/api/v1/auth/login',
+         headers: {
+             'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyYTMwMmIxM2FkZjIxNzA0MzE1MDg0OCIsImlhdCI6MTY1NDg1MDczNywiZXhwIjoxNjU3NDQyNzM3fQ.Q2dxz6pXV308HG7Br94KzbxEHYpaCqZy2MQBv6gHS0M',
+             'Content-Type': 'application/json',
+             // 'Cookie': 'token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyYTMwMmIxM2FkZjIxNzA0MzE1MDg0OCIsImlhdCI6MTY1NDg1MDczNywiZXhwIjoxNjU3NDQyNzM3fQ.Q2dxz6pXV308HG7Br94KzbxEHYpaCqZy2MQBv6gHS0M',
+             'Access-Control-Allow-Origin' : '*'
+         },
+         data : data
+     };
+
+     console.log("1");
+     console.log(data);
+     axios(config)
+         .then(function (response) {
+             console.log("2");
+             console.log(JSON.stringify(response.data));
+         })
+         .catch(function (error) {
+             console.log("3");
+             console.log(error);
+         });
+
+     this.setState({password:"",email:""});
+
     };
     render(){
     return(
         <div style={{textAlign: "center"}}>
             <Typography variant="h6">Contact </Typography>
         <form onSubmit={this.add}>
-            <label className="formdesign">Name</label>
+            <label className="formdesign">Password</label>
             <br></br>
-            <input  type="text" placeholder="Enter your name"  value={this.state.name} onChange={(e)=>this.setState({name :e.target.value})}/>
+            <input  type="text" placeholder="Enter password"  value={this.state.password} onChange={(e)=>this.setState({password :e.target.value})}/>
             <br></br>
             <label className="formdesign">Email</label>
             <br></br>
